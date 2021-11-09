a24832d99fd05da973b468c9ad809d4f4408f9f3
Rogovskiy <ivan.rogovskiy@quantori.com>
2021-11-09 19:21:18 +0300

    button has been made svg

--- script.js
+++ script.js
@@ -1,12 +1,52 @@
-const addButton = document.querySelector('.profile__info-edit-button');
-const editModal = document.querySelector('.popup');
-const closeButton = document.querySelector('.popup__container-close');
+const editProfilePopup = document.querySelector('.popup');
+const closePopupButton = document.querySelector('.popup__container-close');
+const editProfileButton = document.querySelector('.profile__info-edit-button');
+const favPlaceButton = document.querySelector('.place__fav');
+
+const currentName = document.querySelector('.profile__info-name');
+const currentRank = document.querySelector('.profile__info-rank');
+
+const popupInputName = editProfilePopup.querySelector('.popup__container-input-item[id="name"]');
+const popupInputRank = editProfilePopup.querySelector('.popup__container-input-item[id="rank"]');
 
 
 function toggleEditProfileModal() {
-    editModal.classList.toggle('popup_opened')
+    editProfilePopup.classList.toggle('popup_opened')
 }
 
+function initiateProfileInfo() {
+    popupInputName.setAttribute('value', currentName.textContent);
+    popupInputRank.setAttribute('value', currentRank.textContent);
+}
 
-addButton.addEventListener('click', toggleEditProfileModal);
-closeButton.addEventListener('click', toggleEditProfileModal);
+function saveNewProfileInfo(event) {
+    event.preventDefault();
+    currentName.textContent = popupInputName.getAttribute('value');
+    currentRank.textContent = popupInputRank.getAttribute('value');
+    toggleEditProfileModal();
+}
+
+function updateName(e) {
+    popupInputName.setAttribute('value', e.target.value);
+}
+
+function updateRank(e) {
+    popupInputRank.setAttribute('value', e.target.value);
+}
+
+function markAsFavourite() {
+    favPlaceButton.setAttribute('fill', "red");
+
+}
+
+editProfileButton.addEventListener('click', toggleEditProfileModal);
+closePopupButton.addEventListener('click', toggleEditProfileModal);
+
+popupInputName.addEventListener('change', updateName);
+popupInputRank.addEventListener('change', updateRank);
+
+editProfilePopup.addEventListener('submit', saveNewProfileInfo);
+
+favPlaceButton.addEventListener('click', markAsFavourite);
+
+initiateProfileInfo();
